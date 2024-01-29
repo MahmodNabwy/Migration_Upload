3c8847d4177af521dcbb807f44d465b47f673e97
MahmodNabwy <mahmoud.ashraf@digitalhub.com.eg>
2024-01-29 13:00:50 +0200

    Handle Number of records in app settings for testing

--- Services_Repository_Layer/History_Details_Repo.cs
+++ Services_Repository_Layer/History_Details_Repo.cs
@@ -3,6 +3,7 @@ using Context.Old_Models;
 using DTOS.DTO;
 using IServices_Repository_Layer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,25 +15,46 @@ namespace Services_Repository_Layer
     public class History_Details_Repo : GenericRepository<NewCapmasWebsiteContext, HistoryDetail>, IHistory_Details_Repo
     {
         private readonly NewCapmasWebsiteContext _context;
+        private readonly IConfiguration _configuration;
 
-        public History_Details_Repo(NewCapmasWebsiteContext context)
+        public History_Details_Repo(NewCapmasWebsiteContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
         public async Task<List<History_Details_DTO>> GetAllHistoryDetails()
         {
-            var result = await (from q in _context.HistoryDetails.AsNoTracking()
-                                where q.HcsId == 5252 && q.HcaPdfar.EndsWith(".pdf")
-                                select new History_Details_DTO
-                                {
-                                    id = q.HcaId,
-                                    titleAr = q.HcaStatusAr,
-                                    titleEn= q.HcaStatusEn,
-                                    Year = q.HcaStatYear,
-                                    PDF_Ar = q.HcaPdfar,
-                                    PDF_En=q.HcaPdfen,
-                                }).ToListAsync();
-            return result;
+            if (_configuration.GetSection("recordsSize").Value == "")
+            {
+
+                var result = await (from q in _context.HistoryDetails.AsNoTracking()
+                                    where q.HcsId == 5252 && q.HcaPdfar.EndsWith(".pdf")
+                                    select new History_Details_DTO
+                                    {
+                                        id = q.HcaId,
+                                        titleAr = q.HcaStatusAr,
+                                        titleEn = q.HcaStatusEn,
+                                        Year = q.HcaStatYear,
+                                        PDF_Ar = q.HcaPdfar,
+                                        PDF_En = q.HcaPdfen,
+                                    }).ToListAsync();
+                return result;
+            }
+            else
+            {
+                var result = await (from q in _context.HistoryDetails.AsNoTracking()
+                                    where q.HcsId == 5252 && q.HcaPdfar.EndsWith(".pdf")
+                                    select new History_Details_DTO
+                                    {
+                                        id = q.HcaId,
+                                        titleAr = q.HcaStatusAr,
+                                        titleEn = q.HcaStatusEn,
+                                        Year = q.HcaStatYear,
+                                        PDF_Ar = q.HcaPdfar,
+                                        PDF_En = q.HcaPdfen,
+                                    }).Take(int.Parse(_configuration.GetSection("recordsSize").Value)).ToListAsync();
+                return result;
+            }
         }
     }
 }
--- Services_Repository_Layer/OLDNewsRepo.cs
+++ Services_Repository_Layer/OLDNewsRepo.cs
@@ -2,6 +2,7 @@
 using DTOS.DTO;
 using IServices_Repository_Layer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,57 +14,104 @@ namespace Services_Repository_Layer
     public class OLDNewsRepo : GenericRepository<NewCapmasWebsiteContext, TblNews>, IOLDNewsRepo
     {
         private readonly NewCapmasWebsiteContext _context;
+        private readonly IConfiguration _configuration;
 
-        public OLDNewsRepo(NewCapmasWebsiteContext context)
+        public OLDNewsRepo(NewCapmasWebsiteContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
         public async Task<List<OldNewsDTO>> GetAllNews()
         {
-            var reasult = await (from q in _context.TblNews.AsNoTracking()
-                                 select new OldNewsDTO
-                                 {
-                                     id = q.NewsId,
-                                     News_BriefAr = q.NewsBriefAr,
-                                     News_BriefEn = q.NewsBriefEn,
-                                     News_NameAr = q.NewsNameAr,
-                                     News_NameEn = q.NewsNameEn,
-                                     News_Period = q.NewsPeriod,
-                                     News_PublishDate = q.NewsPublishDate
-                                 }).ToListAsync();
-            return reasult;
+
+
+            var result = await (from q in _context.TblNews.AsNoTracking()
+                                select new OldNewsDTO
+                                {
+                                    id = q.NewsId,
+                                    News_BriefAr = q.NewsBriefAr,
+                                    News_BriefEn = q.NewsBriefEn,
+                                    News_NameAr = q.NewsNameAr,
+                                    News_NameEn = q.NewsNameEn,
+                                    News_Period = q.NewsPeriod,
+                                    News_PublishDate = q.NewsPublishDate
+                                }).ToListAsync();
+            return result;
+
         }
         public async Task<List<oldNewsFilesDTO>> GetAllNewsFiles()
         {
-            var result = await (from q in _context.TblNewsFiles.AsNoTracking()
-                                where q.NewsId != null && q.NlLinkAr.EndsWith(".pdf")
-                                select new oldNewsFilesDTO
-                                {
-                                    News_ID = q.NewsId,
-                                    NL_LinkAr = q.NlLinkAr,
-                                    NL_LinkEn = q.NlLinkEn,
-                                    NL_NameAr = q.NlNameAr,
-                                    NL_NameEn = q.NlNameEn,
-                                }).ToListAsync();
-            return result;
+            
+            if (_configuration.GetSection("recordsSize").Value == "")
+            {
+
+                var result = await (from q in _context.TblNewsFiles.AsNoTracking()
+                                    where q.NewsId != null && q.NlLinkAr.EndsWith(".pdf")
+                                    select new oldNewsFilesDTO
+                                    {
+                                        News_ID = q.NewsId,
+                                        NL_LinkAr = q.NlLinkAr,
+                                        NL_LinkEn = q.NlLinkEn,
+                                        NL_NameAr = q.NlNameAr,
+                                        NL_NameEn = q.NlNameEn,
+                                    }).ToListAsync();
+                return result;
+            }
+            else
+            {
+                var result = await (from q in _context.TblNewsFiles.AsNoTracking()
+                                    where q.NewsId != null && q.NlLinkAr.EndsWith(".pdf")
+                                    select new oldNewsFilesDTO
+                                    {
+                                        News_ID = q.NewsId,
+                                        NL_LinkAr = q.NlLinkAr,
+                                        NL_LinkEn = q.NlLinkEn,
+                                        NL_NameAr = q.NlNameAr,
+                                        NL_NameEn = q.NlNameEn,
+                                    }).Take(int.Parse(_configuration.GetSection("recordsSize").Value)).ToListAsync();
+                return result;
+
+            }
         }
         public async Task<List<OldNewsImagesDTO>> GetAllNewsImages()
         {
-            var result = await (from q in _context.TblNews.AsNoTracking()
-                                where q.NewsIsPublished == true && q.NewsVisible == true
+            if (_configuration.GetSection("recordsSize").Value == "")
+            {
 
-                                select new OldNewsImagesDTO
-                                {
-                                    news_id = q.NewsId,
-                                    BriefAr = q.NewsBriefAr,
-                                    BriefEn = q.NewsBriefEn,
-                                    NameAr = q.NewsNameAr,
-                                    NameEn = q.NewsNameEn,
-                                    News_IconLink = q.NewsIconLink,
-                                    PublishDate = q.NewsPublishDate,
+                var result = await (from q in _context.TblNews.AsNoTracking()
+                                    where q.NewsIsPublished == true && q.NewsVisible == true
 
-                                }).ToListAsync();
-            return result;
+                                    select new OldNewsImagesDTO
+                                    {
+                                        news_id = q.NewsId,
+                                        BriefAr = q.NewsBriefAr,
+                                        BriefEn = q.NewsBriefEn,
+                                        NameAr = q.NewsNameAr,
+                                        NameEn = q.NewsNameEn,
+                                        News_IconLink = q.NewsIconLink,
+                                        PublishDate = q.NewsPublishDate,
+
+                                    }).ToListAsync();
+                return result;
+            }
+            else
+            {
+                var result = await (from q in _context.TblNews.AsNoTracking()
+                                    where q.NewsIsPublished == true && q.NewsVisible == true
+
+                                    select new OldNewsImagesDTO
+                                    {
+                                        news_id = q.NewsId,
+                                        BriefAr = q.NewsBriefAr,
+                                        BriefEn = q.NewsBriefEn,
+                                        NameAr = q.NewsNameAr,
+                                        NameEn = q.NewsNameEn,
+                                        News_IconLink = q.NewsIconLink,
+                                        PublishDate = q.NewsPublishDate,
+
+                                    }).Take(int.Parse(_configuration.GetSection("recordsSize").Value)).ToListAsync();
+                return result;
+            }
         }
     }
 }
